b621e38717208afdb3be0cd36976eaf913e7cfe5
林立顏 <id.koier@gmail.com>
2025-03-31 14:15:54 +0800

    2025/03/31復原損失的進度

--- uploadmul.py
+++ uploadmul.py
@@ -1,12 +1,24 @@
 import cv2
 import requests
 import time
+import logging
+import uuid
+
+def get_mac_address():
+    mac = ':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff) for elements in range(0, 2 * 6, 8)][::-1])
+    return mac
+    
+logging.basicConfig(
+    filename="error_log.txt",
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 device_ID = "test_computer"
-device_MAC = "7c:10:c9:a2:50:1a"
+device_MAC = get_mac_address()
 server_ip = "YOUR_IP_ADDRESS"
-server_port = ":YOUR_PORT"
-server_url = "https://"+server_ip+server_port +"/upload_video/" + device_ID
+server_port = "YOUR_PORT"
+server_url = "https://"+server_ip+":"+server_port +"/upload_video/" + device_ID
 
 cap = cv2.VideoCapture(0)
 
@@ -18,6 +30,7 @@ while True:
 
             if not ret:           
                 print("Webcam is not available.")
+                logging.error("Webcam is not available.")
                 time.sleep(5)
                 continue
 
@@ -35,9 +48,11 @@ while True:
                 break
 
     except Exception as e:
-        print("Wait 5 second to restart.",e)
+        error_message = f"Exception occurred: {e}"
+        print("Wait 5 seconds to restart.", error_message)
+        logging.error(error_message) 
         time.sleep(5)
 
-
-cap.release()
-cv2.destroyAllWindows()
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
